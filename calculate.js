ea14aef1ba2860dc4d666c1aef10dbd8b5c940ed
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-02-18 18:34:16 +0600

    Calculate rectangle part

--- calculate.js
+++ calculate.js
@@ -27,33 +27,84 @@ function setTheValueInnerText(number, newValue) {
     
 
 
-
-
-
-
 // Take a event handler
 
-document.getElementById('geometry-container').addEventListener("click", function (event) {
+// document.getElementById('geometry-container').addEventListener("click", function (event) {
 
+
+
+      
+        
+  
+  
+   
+     
+    
+//     }  
+   
+     
+
+//     });
+       
+document.getElementById('triangle-btn').addEventListener('click', function (event) {
     if (event.target.innerText == 'Calculate') {
         const geometryName = event.target.parentNode.parentNode.children[0].innerText;
         // Here, Math variable to Get 'Calculate' text from button click.
 
         const geometryCount = document.querySelectorAll("#area-calculation li");
-     // querySelectorAll for get the all list item from id="area-calculation".
+        // querySelectorAll for get the all list item from id="area-calculation".
         
         //  Count the list item Length.
-        const geometryCountLength =geometryCount.length + 1;
+        const geometryCountLength = geometryCount.length + 1;
         
-        const empty =geometryCountLength + ".  ";
+        const empty = geometryCountLength + ".  ";
         const name = empty + geometryName;
-
-       const triangleForB = getTheValue('triangle-b');
+        const triangleForB = getTheValue('triangle-b');
         const triangleForH = getTheValue('triangle-h');
        
-    const add = triangleForB + triangleForH;
+        const add = triangleForB + triangleForH;
+        // Take a area-calculation list from id="area-calculation".
+        const geometryList = document.getElementById('area-calculation');
+        const li = document.createElement("li");
+        li.id = "newClass";
+        li.innerHTML = `
+        <span >${name}</span><span class="centimeter-btn px-2 " id="innerTextSet">${add}cm<sup>2</sup> <span class="ps-2"> <button  class="btn btn-accent btn-xs">Convert m<sup>2</sup></button></span> 
+         
+         </span>
+    
         
-    // Take a area-calculation list from id="area-calculation".
+        
+        `;
+        const disabled = event.target.setAttribute("disabled", "true");
+        // Change the color of disable button
+     if (disabled != false) {
+         event.target.style.backgroundColor = "rgb(203 213 225)";
+     }
+
+        geometryList.appendChild(li);
+    }
+ })
+document.getElementById('rectangle-btn').addEventListener('click', function (event) {
+    if (event.target.innerText == 'Calculate') {
+        const geometryName = event.target.parentNode.parentNode.children[0].innerText;
+        // Here, Math variable to Get 'Calculate' text from button click.
+
+        const geometryCount = document.querySelectorAll("#area-calculation li");
+        // querySelectorAll for get the all list item from id="area-calculation".
+        
+        //  Count the list item Length.
+        const geometryCountLength = geometryCount.length + 1;
+        
+        const empty = geometryCountLength + ".  ";
+        const name = empty + geometryName;
+
+        // This is part for calculation.
+        const rectangleForW = getTheValue('rectangle-w');
+        const rectangleForI = getTheValue('rectangle-i');
+        // Multiply the  rectangle area
+        const add = rectangleForW * rectangleForI;
+
+        // Take a area-calculation list from id="area-calculation".
         const geometryList = document.getElementById('area-calculation');
         const li = document.createElement("li");
         li.id = "newClass";
@@ -65,35 +116,16 @@ document.getElementById('geometry-container').addEventListener("click", function
         
         
         `;
-       
-      // Here, Append the list items.
-       // li.append(name); class="flex justify-items-center items-center"
-       geometryList.appendChild(li);
-    //     // Here,After select Disable the button.
-    //     const disabled = event.target.setAttribute("disabled", "true");
-    //     // Change the color of disable button
-    //  if (disabled != false) {
-    //      event.target.style.backgroundColor = "rgb(203 213 225)";
-    //  }
+        const disabled = event.target.setAttribute("disabled", "true");
+        // Change the color of disable button
+     if (disabled != false) {
+         event.target.style.backgroundColor = "rgb(203 213 225)";
+     }
 
-   
-         
-    //  const triangleForB = getTheValue('triangle-b');
-    // const triangleForH = getTheValue('triangle-h');
-    // const add = triangleForB + triangleForH;
-    //     setTheValueInnerText('innerTextSet', add)  
-    //     li.innerHTML = 
-    // `    <span class="d-flex items-center justify-between" id="innerTextSet">${$}</span>`
-        
-        
-    //     ;
-    
-    }  
-   
-     
+        geometryList.appendChild(li);
+    }
+ })
 
-    });
-       
 
 
 
