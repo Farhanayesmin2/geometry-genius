92e496681fb4ce1010f54599540cb18874a509d9
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-02-18 23:52:32 +0600

    Random color pointer added

--- calculate.js
+++ calculate.js
@@ -1,32 +1,14 @@
 // Create common function for Calculate function
 // Create a function for Number type input field.
 function getTheValue(number) {
-   
-        const getValueNumber = document.getElementById(number);
-        const getValueNumberString = getValueNumber.value;
-        const getTheNumber = parseInt(getValueNumberString);
- getValueNumber.value = " ";
-   
-         return getTheNumber;
 
+    const getValueNumber = document.getElementById(number);
+    const getValueNumberString = getValueNumber.value;
+    const getTheNumber = parseInt(getValueNumberString);
+    getValueNumber.value = " ";
 
-       
-    
+    return getTheNumber;
 }
-    
-function randomColorAdded() {
-     const cards = document.querySelectorAll('.card');
-              // Generate a random color using Math.random()
-      const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-
-      // Set the background color of the body element
-    cards.style.backgroundColor = randomColor;
-}
- 
-
-
-
-
 
 // Create a function for innerText Value.
 function getTheInnerTextValue(innerValue) {
@@ -49,8 +31,8 @@ function setTheValueInnerText(number, newValue) {
 document
     .getElementById("triangle-btn")
     .addEventListener("click", function (event) {
-      
-      
+
+
         if (event.target.innerText == "Calculate") {
             const geometryName =
                 event.target.parentNode.parentNode.children[0].innerText;
@@ -67,24 +49,17 @@ document
 
             // This part for triangle calculation
             const fixedNumber = 0.5;
-           const triangleForB = getTheValue("triangle-b");
- const triangleForH = getTheValue("triangle-h");
+            const triangleForB = getTheValue("triangle-b");
+            const triangleForH = getTheValue("triangle-h");
 
             const multiplyTriangle = fixedNumber * triangleForB * triangleForH;
 
-           
-// For validation number
-if (Number.isNaN(multiplyTriangle)) {
-  return alert('The value is not a valid number.');
-} 
 
-// added ramdom color
-         const cards = document.querySelector('.card1');
-              // Generate a random color using Math.random()
-      const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            // For validation number
+            if (Number.isNaN(multiplyTriangle)) {
+                return alert('The value is not a valid number.');
+            }
 
-      // Set the background color of the body element
-    cards.style.backgroundColor = randomColor;
 
 
             // Take a area-calculation list from id="area-calculation".
@@ -95,15 +70,10 @@ if (Number.isNaN(multiplyTriangle)) {
         <span >${name}</span><span class="centimeter-btn px-2 " id="innerTextSet">${multiplyTriangle.toFixed(2)}cm<sup>2</sup> <span class="ps-2"> <button  class="btn btn-accent btn-xs">Convert m<sup>2</sup></button></span> 
          
          </span>
-    
-        
-        
         `;
-          
-
             geometryList.appendChild(li);
-            }
-              
+        }
+
     });
 document
     .getElementById("rectangle-btn")
@@ -127,10 +97,10 @@ document
             const rectangleForI = getTheValue("rectangle-i");
             // Multiply the  rectangle area
             const multiplyRectangle = rectangleForW * rectangleForI;
-// For validation number
-if (Number.isNaN(multiplyRectangle)) {
-  return alert('The value is not a valid number.');
-} 
+            // For validation number
+            if (Number.isNaN(multiplyRectangle)) {
+                return alert('The value is not a valid number.');
+            }
 
             // Take a area-calculation list from id="area-calculation".
             const geometryList = document.getElementById("area-calculation");
@@ -141,22 +111,13 @@ if (Number.isNaN(multiplyRectangle)) {
          
          </span>
         `;
-        // added ramdom color
-         const cards = document.querySelector('.card2');
-              // Generate a random color using Math.random()
-      const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-
-      // Set the background color of the body element
-    cards.style.backgroundColor = randomColor;
-  
-
-            geometryList.appendChild(li);
+           geometryList.appendChild(li);
         }
     });
 document
     .getElementById("parallelogram-btn")
     .addEventListener("click", function (event) {
-  
+
         if (event.target.innerText == "Calculate") {
             const geometryName =
                 event.target.parentNode.parentNode.children[0].innerText;
@@ -186,17 +147,7 @@ document
         <span >${name}</span><span class="centimeter-btn pl-1 " id="innerTextSet">${multiplyParallelogram}cm<sup>2</sup> <span class="ps-2"> <button  class="btn btn-accent btn-xs">Convert m<sup>2</sup></button></span> 
          
          </span>
-         
-        
-        
         `;
-          // added ramdom color
-         const cards = document.querySelector('.card3');
-              // Generate a random color using Math.random()
-      const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-
-      // Set the background color of the body element
-    cards.style.backgroundColor = randomColor;
             geometryList.appendChild(li);
         }
     });
@@ -223,7 +174,7 @@ document
             const rhombusFor8 = getTheInnerTextValue("rhombus-8");
 
             // Multiply the Rhombus area
-            const multiplyRhombus =fixedNumber * rhombusFor16 * rhombusFor8;
+            const multiplyRhombus = fixedNumber * rhombusFor16 * rhombusFor8;
 
             // Take a area-calculation list from id="area-calculation".
             const geometryList = document.getElementById("area-calculation");
@@ -233,18 +184,8 @@ document
         <span >${name}</span><span class="centimeter-btn pl-1 " id="innerTextSet">${multiplyRhombus}cm<sup>2</sup> <span class="ps-2"> <button  class="btn btn-accent btn-xs">Convert m<sup>2</sup></button></span> 
          
          </span>
-         
-        
-        
         `;
-           // added ramdom color
-         const cards = document.querySelector('.card4');
-              // Generate a random color using Math.random()
-      const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-
-      // Set the background color of the body element
-    cards.style.backgroundColor = randomColor;
-            geometryList.appendChild(li);
+          geometryList.appendChild(li);  
         }
     });
 document
@@ -270,7 +211,7 @@ document
             const pentagonFor10 = getTheInnerTextValue("pentagon-10");
 
             // Multiply the Pentagon area
-            const multiplyPentagon =fixedNumber * pentagonFor6 * pentagonFor10;
+            const multiplyPentagon = fixedNumber * pentagonFor6 * pentagonFor10;
 
             // Take a area-calculation list from id="area-calculation".
             const geometryList = document.getElementById("area-calculation");
@@ -280,19 +221,8 @@ document
         <span >${name}</span><span class="centimeter-btn pl-1 " id="innerTextSet">${multiplyPentagon}cm<sup>2</sup> <span class="ps-2"> <button  class="btn btn-accent btn-xs">Convert m<sup>2</sup></button></span> 
          
          </span>
-         
-        
-        
         `;
-            // added ramdom color
-         const cards = document.querySelector('.card5');
-              // Generate a random color using Math.random()
-      const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-
-      // Set the background color of the body element
-    cards.style.backgroundColor = randomColor;
-
-            geometryList.appendChild(li);
+           geometryList.appendChild(li); 
         }
     });
 document
@@ -318,7 +248,7 @@ document
             const ellipseFor4 = getTheInnerTextValue("ellipse-4");
 
             // Multiply the Ellipse area
-            const multiplyEllipse =fixedNumberOfPie * ellipseFor10 * ellipseFor4;
+            const multiplyEllipse = fixedNumberOfPie * ellipseFor10 * ellipseFor4;
 
             // Take a area-calculation list from id="area-calculation".
             const geometryList = document.getElementById("area-calculation");
@@ -328,18 +258,75 @@ document
         <span >${name}</span><span class="centimeter-btn pl-1 " id="innerTextSet">${multiplyEllipse.toFixed(2)}cm<sup>2</sup> <span class="ps-2"> <button  class="btn btn-accent btn-xs">Convert m<sup>2</sup></button></span> 
          
          </span>
-         
-        
-        
         `;
-            // added ramdom color
-         const cards = document.querySelector('.card6');
-              // Generate a random color using Math.random()
-      const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-
-      // Set the background color of the body element
-    cards.style.backgroundColor = randomColor;
-
+           
             geometryList.appendChild(li);
         }
     });
+
+
+
+    // Random Color 
+      // added ramdom color card 1
+            const cards = document.querySelector('.card1');
+          cards.addEventListener('mouseover', function() {
+  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  cards.style.backgroundColor = randomColor;
+});
+
+cards.addEventListener('mouseout', function() {
+  cards.style.backgroundColor = '';
+});
+
+             // added ramdom color card 2
+            const card2 = document.querySelector('.card2');
+          card2.addEventListener('mouseover', function() {
+  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  card2.style.backgroundColor = randomColor;
+});
+
+card2.addEventListener('mouseout', function() {
+  card2.style.backgroundColor = '';
+});
+  // added ramdom color card 3
+            const card3 = document.querySelector('.card3');
+          card3.addEventListener('mouseover', function() {
+  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  card3.style.backgroundColor = randomColor;
+});
+
+card3.addEventListener('mouseout', function() {
+  card3.style.backgroundColor = '';
+});
+
+             // added ramdom color card 4
+            const card4 = document.querySelector('.card4');
+          card4.addEventListener('mouseover', function() {
+  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  card4.style.backgroundColor = randomColor;
+});
+
+card4.addEventListener('mouseout', function() {
+  card4.style.backgroundColor = '';
+});
+  // added ramdom color card 5
+            const card5 = document.querySelector('.card5');
+          card5.addEventListener('mouseover', function() {
+  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  card5.style.backgroundColor = randomColor;
+});
+
+card5.addEventListener('mouseout', function() {
+  card5.style.backgroundColor = '';
+});
+
+             // added ramdom color card 2
+            const card6 = document.querySelector('.card6');
+          card6.addEventListener('mouseover', function() {
+  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+  card6.style.backgroundColor = randomColor;
+});
+
+card6.addEventListener('mouseout', function() {
+  card6.style.backgroundColor = '';
+});
